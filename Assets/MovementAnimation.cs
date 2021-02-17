1b24bb51fd14e689e97222297cd3b0366e7d62c8
Ben Mora <blackopsben@gmail.com>
2021-02-17 08:56:19 -0600

    Muzzle flash moves correctly

--- Assets/MovementAnimation.cs
+++ Assets/MovementAnimation.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class MovementAnimation : MonoBehaviour
 {
-    [SerializeField] Animator animator;
+    [SerializeField] Animator spriteAnimator;
+    [SerializeField] Animator muzzlePosAnimator;
     [SerializeField] Transform facingTarget;
     private Movement movement;
+
+    private Vector2[] vectors =
+    {
+        new Vector2(0, 1),
+        new Vector2(0, -1),
+        new Vector2(-1, 0),
+        new Vector2(1, 0)
+    };
 
     private void Start()
     {
@@ -16,13 +25,33 @@ public class MovementAnimation : MonoBehaviour
     private void Update()
     {
         Vector2 vector = facingTarget.position - transform.position;
-        UpdateAnimator(vector);
+        UpdateSpriteAnimator(vector);
+        UpdateMuzzleAnimator(vector);
     }
 
-    public void UpdateAnimator(Vector2 vector)
+    private void UpdateSpriteAnimator(Vector2 vector)
     {
-        animator.SetFloat("Horizontal", vector.x);
-        animator.SetFloat("Vertical", vector.y);
-        animator.SetBool("isMoving", movement.GetIsMoving());
+        spriteAnimator.SetFloat("Horizontal", vector.x);
+        spriteAnimator.SetFloat("Vertical", vector.y);
+        spriteAnimator.SetBool("isMoving", movement.GetIsMoving());
+    }
+
+    private void UpdateMuzzleAnimator(Vector2 vector)
+    {
+        int nearestVectorIndex = 0;
+        float distance = Vector2.Distance(vector, vectors[nearestVectorIndex]);
+
+        for (int i = 1; i < vectors.Length; i++)
+        {
+            float testDistance = Vector2.Distance(vector, vectors[i]);
+            if (testDistance < distance)
+            {
+                nearestVectorIndex = i;
+                distance = testDistance;
+            }
+        }
+
+        muzzlePosAnimator.SetFloat("Horizontal", vectors[nearestVectorIndex].x);
+        muzzlePosAnimator.SetFloat("Vertical", vectors[nearestVectorIndex].y);
     }
 }
