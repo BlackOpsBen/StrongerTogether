77d4693034ae327c331fb1b0242bd7b0fb110310
Ben Mora <blackopsben@gmail.com>
2021-02-17 22:10:08 -0600

    Enemy movement changes blend tree animation

---
+++ Assets/EnemyAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    [SerializeField] AIPath path;
+    [SerializeField] Animator animator;
+
+    private void Update()
+    {
+        animator.SetFloat("Horizontal", path.desiredVelocity.x);
+        animator.SetFloat("Vertical", path.desiredVelocity.y);
+        if (path.desiredVelocity.magnitude > float.Epsilon)
+        {
+            animator.SetBool("isMoving", true);
+        }
+    }
+}
